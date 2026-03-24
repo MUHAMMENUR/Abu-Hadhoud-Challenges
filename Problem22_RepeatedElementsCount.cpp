758255f4c0f84d228dcf47bc661936e199ecb16d
MUHAMMENUR <muhmmednursanda@gmail.com>
2026-03-24 10:22:46 +0300

    refactor: add PrintArr function and improve output formatting for Problem 22

--- Problem22_RepeatedElementsCount.cpp
+++ Problem22_RepeatedElementsCount.cpp
@@ -2,7 +2,10 @@
 #include <string>
 using namespace std;
 
-// Function to fill the array with elements
+
+
+// Function to read and fill the array from the user
+// Note: 'size' is passed by reference (&) to update the original value in main()
 void ReadArray(int Arr[100],int& size){
     cout<<"Enter array elements:";
     cin>>size;
@@ -16,7 +19,8 @@ void ReadArray(int Arr[100],int& size){
     
 
 }
-// Function to count the frequency of a specific number in the array
+// Function to count how many times a specific number appears in the array
+// Returns the count as an integer for reusability
 int CountNumberFrequency(int Arr[100],int size,int NumberToCheck){
     int Counter=0;
 
@@ -30,18 +34,29 @@ int CountNumberFrequency(int Arr[100],int size,int NumberToCheck){
     }
     return Counter;
 }
+void PrintArr(int Arr[],int size){
+    for (int i = 0; i < size; i++)
+    {
+        cout<<Arr[i]<<" ";
+    }
+    
+}
 int main(){
     int Arr[100];
     int arraysize=0;
     // 1. Read the array from the user
     ReadArray( Arr,arraysize);
-// 2. Ask for the number to search for
+    // 2. Ask for the number to search for
     int NumbrToCheck=0;
 
     // 3. Print the final result using the returned value
     cout<<"\nEnter The Number you want to check:";
     cin>>NumbrToCheck;
+
+    PrintArr(Arr,arraysize);
     cout<<"\nNumber "<<NumbrToCheck<<" is reapeted ";
     cout<<CountNumberFrequency(Arr,arraysize,NumbrToCheck)<<" time(s)";
+
+    return 0;
     
 }
